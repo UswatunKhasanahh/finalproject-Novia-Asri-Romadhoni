b5f851d8c67242eec58f3af9af1bed092cdc339f
NoviaAsriRomadhoni29 <noviaasriromadhoni29@gmail.com>
2021-06-23 21:30:41 +0700

    Tambah button di Main Activity

--- app/src/main/java/noviaasriromadhoni/pmo/aplikasi_wisata/MainActivity.java
+++ app/src/main/java/noviaasriromadhoni/pmo/aplikasi_wisata/MainActivity.java
@@ -3,6 +3,8 @@ package noviaasriromadhoni.pmo.aplikasi_wisata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Pesan (View view){
+        Toast.makeText(this, "Ini adalah Tombol Pesan", Toast.LENGTH_SHORT).show();
     }
 }
